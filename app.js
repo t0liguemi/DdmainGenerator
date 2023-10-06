b6aa15c6dde40ad93b94b30e8d8e3e97184b291a
unknown <toliguemi94@gmail.com>
2023-10-06 19:34:30 -0300

    correcion de declaraciones

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-pronouns=["the","some","your","that","certain"]
-adjectives=["hideous","insightful","charismatic","hopeless","vicious","derogatory","viscous"]
-substantives=["airplane","landmass","village","squid","armchair","bonelesschicken"]
-domains=[".com",".cl",".us",".gov",".eu",".de",".io"]
-
-for(pronoun of pronouns){
-    for(adjective of adjectives){
-        for(substantive of substantives){
-            for(domain of domains){
+const pronouns=["the","some","your","that","certain"]
+const adjectives=["hideous","insightful","charismatic","hopeless","vicious","derogatory","viscous"]
+const substantives=["airplane","landmass","village","squid","armchair","bonelesschicken"]
+const domains=[".com",".cl",".us",".gov",".eu",".de",".io"]
+for(let pronoun of pronouns){
+    for(let adjective of adjectives){
+        for(let substantive of substantives){
+            for(let domain of domains){
                 console.log(pronoun+adjective+substantive+domain)
             }
         }
